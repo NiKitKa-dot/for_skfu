d70848921bfe4332f8f55e3530c2afe0976554f7
NiKitKa-dot <84975159+NiKitKa-dot@users.noreply.github.com>
2025-03-10 22:24:27 +0300

    Update 4task.cpp

--- 3laba/4task.cpp
+++ 3laba/4task.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <map>
 #include <set>
+#include <cstdlib>
 using namespace std;
 
 int main() {
+	system("chcp 1251");
 	//создаём словарь
 	map <char, int> dict;
-	string user_input;
+	string user_input, answer= "";
 	getline(cin, user_input);
 	set <char> s;
 	for (char i : user_input) {
@@ -21,5 +23,16 @@ int main() {
 		dict[i] += 1;
 	}
 
-	for (auto i : dict) cout << i.first << "  " << i.second<<"\n";
+	for (auto i : dict) cout <<'"' << i.first << '"'<<" встречается " << i.second << " раз(а)\n";
+	//замена всех слов «уникальный» на слова «единственный» 
+	int i = 0;
+	while ( i < user_input.size()-10) {
+		if (user_input.substr(i, 10) == "уникальный") {
+			answer += "единственный";
+			i += 9;
+		}
+		answer += user_input[i];
+		i++;
+	}
+	cout << answer+ user_input.substr(user_input.size() - 10, 10);
 }
